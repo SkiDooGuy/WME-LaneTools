424a01af22a9d5ca16318f6448fe44a6e8ab5199
mveygman <mveygman@gmail.com>
2023-08-26 18:50:52 -0400

    Multiple deobfuscation and fixes.

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/** @type {import("prettier").Config} */
+const config = {
+    trailingComma: "es5",
+    tabWidth: 2,
+    semi: false,
+    singleQuote: true,
+    printWidth: 120,
+};
+
+export default config;
