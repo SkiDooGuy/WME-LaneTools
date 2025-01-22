c2d752ef7f18affa0424d974fe6421fdf7e0323d
jm6087 <jm6087waze@gmail.com>
2025-01-21 20:35:53 -0600

    Update WME-LaneTools.user.js

--- WME-LaneTools.user.js
+++ WME-LaneTools.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME LaneTools
 // @namespace    https://github.com/SkiDooGuy/WME-LaneTools
-// @version      2025.01.20.01
+// @version      2025.01.21.01
 // @description  Adds highlights and tools to WME to supplement the lanes feature
 // @author       SkiDooGuy, Click Saver by HBiede, Heuristics by kndcajun, assistance by jm6087
 // @updateURL    https://github.com/SkiDooGuy/WME-LaneTools/raw/master/WME-LaneTools.user.js
